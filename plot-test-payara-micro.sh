cadcd9e0a57d41d9d081f71aed5583064b29c7c1
Harald Reinmueller (FG-4-I-1) <Harald.RA.Reinmueller@bmwgroup.com>
2019-11-11 16:32:18 +0100

    added simple demo projects

--- plot-test-payara-micro.sh
+++ plot-test-payara-micro.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./plot-test.sh "java -Xmn16m -Xmx512m -jar ../demo2/target/demo2-microbundle.jar" http://localhost:8080/hello myplot-payara-micro.png
+./plot-test.sh "java -Xmn16m -Xmx512m -jar demo-payara/target/demo-payara-microbundle.jar" http://localhost:8080/hello myplot-payara-micro.png
