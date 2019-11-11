d822b4638aaff894118bf1bfc2afbd048ad2a1d4
Harald Reinmueller (FG-4-I-1) <Harald.RA.Reinmueller@bmwgroup.com>
2019-11-11 16:10:49 +0100

    work with linux

--- plot-test.sh
+++ plot-test.sh
@@ -1,14 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 echo $@
 
 $1 &
 MY_PID=$!
 sleep 0.02
 psrecord $MY_PID --plot $3 & PSRECORD_PID=$!
-sleep 1
 
 # sleep until ready
-while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' $2)" != "200" ]]; do sleep .00001; done
+while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' $2)" != "200" ]]; do sleep 0.02; done
+echo "##### FIRST SERVED REQUEST #####"
 
 for i in {1..3}
 do
@@ -17,6 +17,4 @@ do
 done
 
 kill -9 $MY_PID
-
-echo "\n"
 echo $(pwd)/$3
